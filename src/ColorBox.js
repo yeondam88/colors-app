e4ebd42526e9120e63b12fde30e0ce5c6241dd22
Yeondam Park <giyen9088@gmail.com>
2020-03-04 23:59:47 -0800

    Add Color Overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 function ColorBox({ color, name }) {
+  const [copied, setCopied] = useState(false);
+
+  const copyHandler = useCallback(() => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  }, []);
+
   return (
-    <div style={{ backgroundColor: color }} className="ColorBox">
-      <div className="copy-container">
-        <div className="box-content">
-          <span>{name}</span>
+    <CopyToClipboard text={color} onCopy={copyHandler}>
+      <div style={{ backgroundColor: color }} className="ColorBox">
+        <div
+          style={{ backgroundColor: color }}
+          className={`copy-overlay ${copied && 'show'}`}
+        ></div>
+        <div className={`copy-msg ${copied && 'show'}`}>
+          <h1>Copied!</h1>
+          <p>{color}</p>
         </div>
-        <button className="copy-button">Copy</button>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
-      <span className="see-more">More</span>
-    </div>
+    </CopyToClipboard>
   );
 }
 
