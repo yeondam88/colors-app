db82ccb3068cb9175b4db8c6460987480075294a
Yeondam Park <giyen9088@gmail.com>
2020-03-06 23:45:29 -0800

    Adding material-ui

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
-function Navbar({ level, onChange }) {
+function Navbar({ level, onChange, handleChange }) {
+  const [format, setFormat] = useState('hex');
+  const handleFormatChange = useCallback(
+    e => {
+      setFormat(e.target.value);
+      handleChange(format);
+    },
+    [format, handleChange]
+  );
   return (
     <header className="Navbar">
       <div className="logo">
@@ -20,6 +30,13 @@ function Navbar({ level, onChange }) {
             onAfterChange={onChange}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select value={format} onChange={handleFormatChange}>
+          <MenuItem value="hex">Hex - #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA - rgb(255,255,255,0.1)</MenuItem>
+        </Select>
       </div>
     </header>
   );
--- src/Palette.js
+++ src/Palette.js
@@ -6,8 +6,13 @@ import './Palette.css';
 
 function Palette(props) {
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState('hex');
   const onChange = useCallback(newLevel => {
     setLevel(newLevel);
+  }, []);
+
+  const handleChange = useCallback(format => {
+    setFormat(format);
   }, []);
 
   const colorBoxes = props.palette.colors[level].map(color => {
@@ -17,7 +22,7 @@ function Palette(props) {
   });
   return (
     <div className="Palette">
-      <Navbar level={level} onChange={onChange} />
+      <Navbar level={level} onChange={onChange} handleChange={handleChange} />
       <div className="Palette-colors">{colorBoxes}</div>
     </div>
   );
