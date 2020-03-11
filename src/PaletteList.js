6be1dac52b7b73716fc11bc271103970157691dc
Yeondam Park <giyen9088@gmail.com>
2020-03-10 22:18:06 -0700

    styling mini palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import MiniPalette from './MiniPalette';
 import { Link } from 'react-router-dom';
 
 function PaletteList({ palettes }) {
   return (
     <div>
       {palettes.map(palette => {
+        console.log(palette);
         return (
-          <Link to={`/palette/${palette.id}`}>
-            <h1>{palette.paletteName}</h1>
-          </Link>
+          <div>
+            <MiniPalette {...palette} />
+            <Link to={`/palette/${palette.id}`}>
+              <h1>{palette.paletteName}</h1>
+            </Link>
+          </div>
         );
       })}
     </div>
